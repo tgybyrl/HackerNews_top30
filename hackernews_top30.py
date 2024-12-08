7a412e27cb7d80cedd40e30d5014f7d99ea06cec
Tugay Bayırlı <tgybyrl48@gmail.com>
2024-12-08 23:56:17 +0200

    succeed to get urls and names from top 30 news

--- hackernews_top30.py
+++ hackernews_top30.py
@@ -3,14 +3,14 @@ import json
 from bs4 import BeautifulSoup
 
 hacker_news_url = "https://news.ycombinator.com/news"
-title_list = []
 
 response = requests.get(hacker_news_url)
 print(response.status_code) #to check my status
-
 soup = BeautifulSoup(response.text, "html.parser")
 
-title_link = soup.find_all("span", class_="titleline")
-for titles in title_link:
-    title = titles.text  # Get the title text
-    print(f"title: {title} ")
+
+news_title_and_url = soup.find_all("span", class_="titleline")
+for titles in news_title_and_url:
+    title = titles.a.text # Get the title text
+    url = titles.a.get("href")
+    print(f"title: {title}\nurl: {url}")
